3ca17a8749afc1c7ea49eca28dd040580e58dd1d
Maxime Simon <simon.maxime@gmail.com>
2014-11-11 21:55:45 +0100

    feat: Add FlexGet start/stop controls (not implemented yet).

--- public/logs.js
+++ public/logs.js
@@ -1,24 +1,28 @@
 angular.module('goFlexGet')
     .controller('LogsCtrl', ['$scope', '$http', '$sce', '$timeout',
         function($scope, $http, $sce, $timeout) {
-            $scope.logsLoading = true;
-            $http.get('/api/logs')
-                .success(function(data) {
-                    $scope.logsLoading = false;
-                    $scope.flexgetLogs = data;
-                    // Wait for template before scrolling to bottom
-                    $timeout(function() {
-                        $('#logs').animate({
-                            'scrollTop': $('#logs')[0].scrollHeight
-                        }, 100);
+            $scope.getLogs = function() {
+                $scope.logsLoading = true;
+                $http.get('/api/logs')
+                    .success(function(data) {
+                        $scope.logsLoading = false;
+                        $scope.flexgetLogs = data;
+                        // Wait for template before scrolling to bottom
+                        $timeout(function() {
+                            $('#logs').animate({
+                                'scrollTop': $('#logs')[0].scrollHeight
+                            }, 100);
+                        });
+                    })
+                    .error(function(data, status) {
+                        $scope.logsLoading = false;
+                        var data = data || "Request failed";
+                        $scope.logsError = $sce.trustAsHtml(
+                            '<strong>Unable to retrieve FlexGet logs:</strong> ' + data +
+                            ' (' +
+                            status + ')');
                     });
-                })
-                .error(function(data, status) {
-                    $scope.logsLoading = false;
-                    var data = data || "Request failed";
-                    $scope.logsError = $sce.trustAsHtml(
-                        '<strong>Unable to retrieve FlexGet logs:</strong> ' + data + ' (' +
-                        status + ')');
-                });
+            };
+            $scope.getLogs();
         }
     ]);
--- public/main.js
+++ public/main.js
@@ -28,18 +28,53 @@ angular.module('goFlexGet', ['ngRoute'])
 
 .controller('MainCtrl', ['$scope', '$http',
     function($scope, $http) {
-        $scope.statusLoading = true;
-        $http.get('/api/status')
-            .success(function(data) {
-                $scope.statusLoading = false;
-                $scope.fgStatus = data;
-            })
-            .error(function(data, status) {
-                $scope.statusLoading = false;
-                var data = data || "Request failed";
-                $scope.configError = $sce.trustAsHtml(
-                    '<strong>Unable to retrieve FlexGet status:</strong> ' + data +
-                    ' (' + status + ')');
-            });
+        $scope.getStatus = function() {
+            $scope.statusLoading = true;
+            $http.get('/api/status')
+                .success(function(data) {
+                    $scope.statusLoading = false;
+                    $scope.fgStatus = data;
+                })
+                .error(function(data, status) {
+                    $scope.statusLoading = false;
+                    var data = data || "Request failed";
+                    $scope.statusError = $sce.trustAsHtml(
+                        '<strong>Unable to retrieve FlexGet status:</strong> ' + data +
+                        ' (' + status + ')');
+                });
+        };
+        $scope.getStatus();
+
+        $scope.isStarting = false;
+        $scope.startFlexGet = function() {
+            $scope.isStarting = true;
+            $http.get('/api/start')
+                .success(function(data) {
+                    $scope.getStatus();
+                    $scope.isStarting = false;
+                })
+                .error(function(data, status) {
+                    var data = data || "Request failed";
+                    console.error(data)
+                    $scope.getStatus();
+                    $scope.isStarting = false;
+                });
+        };
+
+        $scope.isStopping = false;
+        $scope.stopFlexGet = function() {
+            $scope.isStopping = true;
+            $http.get('/api/stop')
+                .success(function(data) {
+                    $scope.getStatus();
+                    $scope.isStopping = false;
+                })
+                .error(function(data, status) {
+                    var data = data || "Request failed";
+                    console.error(data)
+                    $scope.getStatus();
+                    $scope.isStopping = false;
+                });
+        };
     }
 ]);
