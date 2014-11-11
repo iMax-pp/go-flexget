f96dc27701270b93e1e7ff150f34de26c1a4446a
Maxime Simon <simon.maxime@gmail.com>
2014-11-11 15:45:22 +0100

    style: Beautify JS and HTML. Apply prettify to Config.

--- public/main.js
+++ public/main.js
@@ -1,51 +1,67 @@
 angular.module('goFlexGet', ['ngRoute'])
-.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        controller:'MainCtrl',
-        templateUrl:'/main.html'
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'MainCtrl',
+                templateUrl: '/main.html'
+            })
+            .when('/config', {
+                controller: 'ConfigCtrl',
+                templateUrl: '/config.html'
+            })
+            .when('/logs', {
+                controller: 'LogsCtrl',
+                templateUrl: '/logs.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
     })
-    .when('/config', {
-        controller:'ConfigCtrl',
-        templateUrl:'/config.html'
-    })
-    .when('/logs', {
-        controller:'LogsCtrl',
-        templateUrl:'/logs.html'
-    })
-    .otherwise({
-        redirectTo:'/'
-    });
-})
 
-.controller('HeaderCtrl', ['$scope', '$location', function($scope, $location) {
-    $scope.isActive = function(viewLocation) {
-        return viewLocation === $location.path();
-    };
-}])
+.controller('HeaderCtrl', ['$scope', '$location',
+    function($scope, $location) {
+        $scope.isActive = function(viewLocation) {
+            return viewLocation === $location.path();
+        };
+    }
+])
 
-.controller('MainCtrl', ['$scope', function($scope) {
-}])
+.controller('MainCtrl', ['$scope',
+    function($scope) {}
+])
 
-.controller('ConfigCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-    $http.get('/api/config').
-    success(function(data) {
-        $scope.flexgetConfig = data;
-    }).
-    error(function(data, status) {
-        var data = data || "Request failed";
-        $scope.retrieveError = $sce.trustAsHtml('<strong>Unable to retrieve FlexGet configuration:</strong> ' + data + ' (' + status + ')');
-    });
-}])
+.controller('ConfigCtrl', ['$scope', '$http', '$sce',
+    function($scope, $http, $sce) {
+        $http.get('/api/config')
+            .success(function(data) {
+                $scope.flexgetConfig = data;
+            })
+            .error(function(data, status) {
+                var data = data || "Request failed";
+                $scope.retrieveError = $sce.trustAsHtml(
+                    '<strong>Unable to retrieve FlexGet configuration:</strong> ' + data +
+                    ' (' + status + ')');
+            });
+    }
+])
 
-.controller('LogsCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-    $http.get('/api/logs').
-    success(function(data) {
-        $scope.flexgetLogs = data;
-        $('#logs').animate({'scrollTop': $('#logs')[0].scrollHeight}, 100);
-    }).
-    error(function(data, status) {
-        var data = data || "Request failed";
-        $scope.retrieveError = $sce.trustAsHtml('<strong>Unable to retrieve FlexGet logs:</strong> ' + data + ' (' + status + ')');
-    });
-}]);
+.controller('LogsCtrl', ['$scope', '$http', '$sce', '$timeout',
+    function($scope, $http, $sce, $timeout) {
+        $http.get('/api/logs')
+            .success(function(data) {
+                $scope.flexgetLogs = data;
+                // Wait for template before scrolling to bottom
+                $timeout(function() {
+                    $('#logs').animate({
+                        'scrollTop': $('#logs')[0].scrollHeight
+                    }, 100);
+                });
+            })
+            .error(function(data, status) {
+                var data = data || "Request failed";
+                $scope.retrieveError = $sce.trustAsHtml(
+                    '<strong>Unable to retrieve FlexGet logs:</strong> ' + data + ' (' +
+                    status + ')');
+            });
+    }
+]);
